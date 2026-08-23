0db5c7633baaa9e284997c805c6db2f31eb9a587
ViditzPandey <viditzpandey001@gmail.com>
2026-08-24 00:29:38 +0530

    reviews updated

--- models/listing.js
+++ models/listing.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const Review = require("./review.js");
 
 const listingSchema = new Schema({
     title: {
@@ -9,16 +10,22 @@ const listingSchema = new Schema({
     description: String,
     image: {
         type: String,
-        default:"https://unsplash.com/photos/a-house-on-the-beach-with-palm-trees-in-the-background-0uq8ZY9jyQo",
+        default: "https://unsplash.com/photos/a-house-on-the-beach-with-palm-trees-in-the-background-0uq8ZY9jyQo",
         set: (v) => v === "" ? "https://unsplash.com/photos/a-house-on-the-beach-with-palm-trees-in-the-background-0uq8ZY9jyQo" : v
     },
     price: Number,
     location: String,
     country: String,
-    reviews:[{
-        type:Schema.Types.ObjectId,
-        ref:"Review",
+    reviews: [{
+        type: Schema.Types.ObjectId,
+        ref: "Review",
     }]
+});
+
+listingSchema.post("findOneAndDelete", async (listing) => {
+    if (listing) {
+        await Review.deleteMany({ _id: { $in: listing.reviews } });
+    }
 });
 
 const Listing = mongoose.model("Listing", listingSchema);
